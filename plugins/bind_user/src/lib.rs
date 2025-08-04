07c21b344ffe3882d39aa14484564de30f2a1a82
mokurin000 <1348292515a@gmail.com>
2025-08-05 00:20:35 +0800

    feat: implement user bind

--- plugins/bind_user/src/lib.rs
+++ plugins/bind_user/src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(more_qualified_paths)]
+#![feature(if_let_guard)]
 
 use kovi::{Message, MsgEvent, PluginBuilder as plugin};
 use nyquest_preset::nyquest::AsyncClient;
@@ -13,12 +14,14 @@ use sdgb_api::{
 use serde::Serialize;
 use shared_client::nyquest_client;
 use spdlog::error;
+use userdb::unbind_user;
 
 #[kovi::plugin]
 async fn start() {
     let client = nyquest_client().await;
 
     plugin::on_msg(async move |event| {
+        let sender_id = event.user_id;
         let text = event.borrow_text()?;
         let segments = text.split_whitespace().collect::<Vec<&str>>();
 
@@ -33,21 +36,51 @@ async fn start() {
                 }
                 reply_event(event, m);
             }
+            &["/unbind"] => match unbind_user(sender_id) {
+                Err(e) => {
+                    reply_event(event, "解绑失败~ 请联系管理员或稍后重试");
+                    error!("redb error: {e}");
+                }
+                Ok(removed) => reply_event(
+                    event,
+                    if removed {
+                        "解绑完成~"
+                    } else {
+                        "目前还没有绑定喵~"
+                    },
+                ),
+            },
+            &["/bind", user_id] if let Ok(user_id) = user_id.parse::<u32>() => {
+                if let Ok(preview) = preview_user(client, user_id).await {
+                    if let Ok(Some(_)) = userdb::record_userid(sender_id, user_id) {
+                        reply_event(event, "目前已绑定用户了喵~ 使用 /unbind 来解绑哦");
+                    } else {
+                        reply_event(
+                            event,
+                            Message::new()
+                                .add_text(format!("已成功绑定到 userId {user_id}\n\n{preview}")),
+                        );
+                    }
+                } else {
+                    reply_event(event, "无效的 userId ~");
+                }
+            }
             &["/bind", qrcode_content] => {
                 match (QRCode { qrcode_content }).login(client).await {
                     Ok(user_id) => {
-                        let sender_id = event.user_id;
                         let user_id = user_id as u32;
 
-                        if let Err(e) = userdb::record_userid(sender_id, user_id) {
-                            reply_event(event, "绑定失败~ 请联系管理员或稍后重试");
-                            error!("redb error: {e}");
-                        } else {
-                            let mut message = Message::new().add_text("绑定成功~ ❤");
-                            if let Ok(preview) = preview_user(client, user_id).await {
-                                message = message.add_text("\n\n").add_text(&preview);
+                        match userdb::record_userid(sender_id, user_id) {
+                            Err(e) => {
+                                reply_event(event, "绑定失败~ 请联系管理员或稍后重试");
+                                error!("redb error: {e}");
                             }
-                            reply_event(event, message);
+                            Ok(Some(_)) => {
+                                reply_event(event, "目前已绑定用户了喵~ 使用 /unbind 来解绑哦");
+                            }
+                            Ok(None) => {
+                                bind_user_id(client, event, user_id).await;
+                            }
                         }
                     }
                     Err(QRLoginError::QRCodeExpired10) => {
@@ -99,4 +132,12 @@ where
     } else {
         event.reply(msg);
     }
+}
+
+async fn bind_user_id(client: &AsyncClient, event: impl AsRef<MsgEvent>, user_id: u32) {
+    let mut message = Message::new().add_text("绑定成功~ ❤");
+    if let Ok(preview) = preview_user(client, user_id).await {
+        message = message.add_text("\n\n").add_text(&preview);
+    }
+    reply_event(event, message);
 }
--- userdb/src/lib.rs
+++ userdb/src/lib.rs
@@ -1,6 +1,6 @@
 use std::sync::LazyLock;
 
-use redb::{Database, TableDefinition, WriteTransaction};
+use redb::{Database, ReadableTable as _, TableDefinition, WriteTransaction};
 use spdlog::{error, info};
 
 const QQ_USERID: TableDefinition<'_, i64, u32> = TableDefinition::new("userid");
@@ -19,13 +19,29 @@ pub fn write_txn() -> Result<WriteTransaction, redb::Error> {
     Ok(DATABASE.begin_write()?)
 }
 
-pub fn record_userid(id: i64, user_id: u32) -> Result<(), redb::Error> {
+pub fn unbind_user(id: i64) -> Result<bool, redb::Error> {
     let txn = write_txn()?;
+    let result;
     {
         let mut table = txn.open_table(QQ_USERID)?;
-        table.insert(id, user_id)?;
+        result = table.remove(id)?.is_some();
     }
 
     txn.commit()?;
-    Ok(())
+    Ok(result)
+}
+
+pub fn record_userid(id: i64, user_id: u32) -> Result<Option<u32>, redb::Error> {
+    let txn = write_txn()?;
+    {
+        let mut table = txn.open_table(QQ_USERID)?;
+        if let Some(uid) = table.get(id)? {
+            return Ok(Some(uid.value()));
+        }
+        table.insert(id, user_id)?;
+        info!("new record: {id} <=> {user_id}");
+    }
+
+    txn.commit()?;
+    Ok(None)
 }
