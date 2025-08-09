0b0bf4e81e339dfd520c2f5e17e5f99bed281975
mokurin000 <1348292515a@gmail.com>
2025-08-09 19:30:18 +0800

    refactor: dedup different x_scale
    thanks to @Aalivexy

--- plugins/pcrt-plot/src/plot.rs
+++ plugins/pcrt-plot/src/plot.rs
@@ -1,7 +1,11 @@
 use std::error::Error;
 
 use glam::U8Vec3;
-use plotters::{prelude::*, style::full_palette::LIGHTBLUE};
+use plotters::{
+    coord::ranged1d::{AsRangedCoord, DefaultFormatting},
+    prelude::*,
+    style::full_palette::LIGHTBLUE,
+};
 
 fn colormap(rating: i32) -> RGBColor {
     // from zrender fastlerp
@@ -22,7 +26,6 @@ fn colormap(rating: i32) -> RGBColor {
 
     RGBColor(x, y, z)
 }
-
 pub fn draw_chart(
     image_path: impl AsRef<std::path::Path>,
     xy_data: impl IntoIterator<Item = (i32, i32)>,
@@ -32,47 +35,51 @@ pub fn draw_chart(
     last_x: i32,
     is_log_x: bool,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if is_log_x {
+        let x_spec = x_min..x_max;
+        draw_chart_impl(image_path, xy_data, x_spec, y_max, last_x)
+    } else {
+        let x_spec = (x_min..x_max).log_scale();
+        draw_chart_impl(image_path, xy_data, x_spec, y_max, last_x)
+    }
+}
+
+pub fn draw_chart_impl<X, R>(
+    image_path: impl AsRef<std::path::Path>,
+    xy_data: impl IntoIterator<Item = (i32, i32)>,
+    x_spec: X,
+    y_max: i32,
+    last_x: i32,
+) -> Result<(), Box<dyn Error + Send + Sync>>
+where
+    X: AsRangedCoord<Value = i32, CoordDescType = R>,
+    R: Ranged<FormatOption = DefaultFormatting, ValueType = i32>,
+{
     let root = BitMapBackend::new(image_path.as_ref(), (1600, 1000)).into_drawing_area();
     root.fill(&WHITE)?;
 
-    let x_range = x_min..x_max;
     let y_range = 50..y_max;
 
-    let mut builder = ChartBuilder::on(&root);
-    let chart = builder
+    let mut chart = ChartBuilder::on(&root)
         .margin(40)
         .x_label_area_size(80)
-        .y_label_area_size(75);
+        .y_label_area_size(75)
+        .build_cartesian_2d(x_spec, y_range)?;
 
     let label_style = ("sans-serif", 23, &LIGHTBLUE).into_text_style(&root);
     let desc_style = ("sans-serif", 30, &BLACK).into_text_style(&root);
     let title_style = ("sans-serif", 40, &BLACK).into_text_style(&root);
 
-    if is_log_x {
-        let mut chart = chart.build_cartesian_2d(x_range.log_scale(), y_range)?;
-        chart
-            .configure_mesh()
-            .y_labels(((y_max + 9) / 10) as _)
-            .x_label_style(label_style.clone())
-            .y_label_style(label_style.clone())
-            .draw()?;
-        // draw points
-        chart.draw_series(xy_data.into_iter().map(|(x, y)| {
-            Circle::new((x, y), 2.5, colormap(y).filled()) // 点大小为 5
-        }))?;
-    } else {
-        let mut chart = chart.build_cartesian_2d(x_range, y_range)?;
-        chart
-            .configure_mesh()
-            .y_labels(((y_max + 9) / 10) as _)
-            .x_label_style(label_style.clone())
-            .y_label_style(label_style.clone())
-            .draw()?;
-        // draw points
-        chart.draw_series(xy_data.into_iter().map(|(x, y)| {
-            Circle::new((x, y), 2.5, colormap(y).filled()) // 点大小为 5
-        }))?;
-    };
+    chart
+        .configure_mesh()
+        .y_labels(((y_max + 9) / 10) as _)
+        .x_label_style(label_style.clone())
+        .y_label_style(label_style.clone())
+        .draw()?;
+    // draw points
+    chart.draw_series(xy_data.into_iter().map(|(x, y)| {
+        Circle::new((x, y), 2.5, colormap(y).filled()) // 点大小为 5
+    }))?;
 
     // add desc
     root.draw_text(&format!("Total play: {last_x}"), &desc_style, (100, 950))?;
